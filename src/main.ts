ccb88f3e2a79fd1b470686d3fc34f50f390144f7
Hosam <hosam.abdelaty2015@gmail.com>
2024-03-24 06:54:27 +0200

    port

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,3 @@
-/* eslint-disable prettier/prettier */
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors({
-//     origin: 'http://localhost:3000',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-//   });
-//   await app.listen(3001);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
@@ -20,6 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3001;
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
     origin: 'http://localhost:3000', // Allow requests from this origin
@@ -27,6 +13,6 @@ async function bootstrap() {
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   };
   app.enableCors(corsOptions);
-  await app.listen(3001);
+  await app.listen(PORT);
 }
 bootstrap();
